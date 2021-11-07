c3a83bb9b5d1cc49a36a0336a00ba1e9e6057885
Nicholas Brown <nickterp@gmail.com>
2021-11-06 20:50:31 -0400

    changing the name, adding default vpc

--- prod.tf
+++ prod.tf
@@ -3,8 +3,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-20211105-nbrown"
   acl 	 = "private"
 }
+
+resource "aws_default_vpc" "default" {}
+
+
 
