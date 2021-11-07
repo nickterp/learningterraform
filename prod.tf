5d26d0257b38e35bf2b6d74264341f61f743d755
Nicholas Brown <nickterp@gmail.com>
2021-11-07 10:53:31 -0500

    split out into var file

--- prod.tf
+++ prod.tf
@@ -1,3 +1,23 @@
+variable "allow_list" {
+  type = list(string)
+}
+variable "web_image_id" {
+  type = string
+}
+variable "web_instance_type" {
+  type = string
+}
+variable "web_desired_capacity" {
+  type = number
+}
+variable "web_max_size" {
+  type = number
+}
+variable "web_min_size" {
+  type = number
+}   
+
+
 provider "aws" {
   profile = "default"
   region = "us-east-2"
@@ -34,7 +54,7 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["136.56.25.99/32"]
+    cidr_blocks = var.allow_list
     self        = "true"
   }
 
@@ -42,7 +62,7 @@ resource "aws_security_group" "prod_web" {
     from_port   = 443 
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["136.56.25.99/32"]
+    cidr_blocks = var.allow_list
     self        = "true"
   }
 
@@ -58,35 +78,8 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami           = "ami-04fba448246555019"
-  instance_type = "t2.nano"
-  
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web[0].id
-  allocation_id = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
 resource "aws_elb" "prod_web" {
   name            = "prod-web"
-  instances       = aws_instance.prod_web[*].id
   subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
@@ -102,3 +95,45 @@ resource "aws_elb" "prod_web" {
   }
 }
 
+resource "aws_launch_template" "prod_web" {
+  name_prefix            = "prod-web"
+  image_id               = var.web_image_id
+  instance_type          = var.web_instance_type
+  vpc_security_group_ids = [aws_security_group.prod_web.id]
+
+  tags = {
+    "Terraform" : "true"
+  }   
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity    = var.web_desired_capacity
+  max_size            = var.web_max_size
+  min_size            = var.web_min_size
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      load_balancers,
+      target_group_arns
+    ]
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
+}
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+allow_list           = ["136.56.25.99/32"]
+web_image_id         = "ami-04fba448246555019"
+web_instance_type    = "t2.micro"
+web_desired_capacity = 1
+web_max_size         = 1
+web_min_size         = 1
